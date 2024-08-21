6c272bf9f43c1c8a35f6ca7413e916c28ac9591b
Ash Johnston <john3849@pnw.net>
2024-08-21 15:30:44 -0500

    TreeBrush

---
+++ src/TreeBrush.java
@@ -0,0 +1,11 @@
+public class TreeBrush {
+    public static void main(String[] args) {
+        System.out.println("     o");
+        System.out.println("   oooo");
+        System.out.println("  ooooooo");
+        System.out.println("  oooooooo");
+        System.out.println("   ooIIo o       ooo");
+        System.out.println("   o IIo        oooooo");
+        System.out.println("_____II_________ oooo____");
+    }
+}
