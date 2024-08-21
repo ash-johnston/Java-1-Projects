7c2c12342e4817e02bb9ef69b555da44d9a3eeeb
Ash Johnston <john3849@pnw.net>
2024-08-21 15:35:02 -0500

    FixMe

---
+++ src/FixMe.java
@@ -0,0 +1,9 @@
+public class FixMe {
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("*      *");
+        System.out.println("* Java *");
+        System.out.println("*      *");
+        System.out.println("********");
+    }
+}
