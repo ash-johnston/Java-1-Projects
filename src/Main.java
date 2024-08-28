b2ba9585623cb4f1206c431f78187c4981f20929
Ash Johnston <john3849@pnw.net>
2024-08-28 12:26:28 -0500

    my awesome store

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
+    private static final String STORE_NAME = "My awesome store!";
     public static void main(String[] args) {
-        System.out.println(" /\\_/\\     ----- ");
-        System.out.println("( ' ' )  / Hello \\");
-        System.out.println("(  -  ) <  Junior |");
-        System.out.println(" | | |   \\ Coder!/");
-        System.out.println("(__|__)    -----");
+        String productName = "Laptop";
+        int id = 8753874;
+        double price = 3199.99;
+        int available = 35;
+        System.out.println(STORE_NAME);
+        System.out.println("\nProduct Information:");
+        System.out.println("Product Name: " + productName);
+        System.out.println("Product ID: " + id);
+        System.out.println("Product Price: $" + price);
+        System.out.println("Quantity Available: " + available);
     }
 }
